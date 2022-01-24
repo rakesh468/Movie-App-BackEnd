a3fae3fc9c53bc42e9458ed883a6e10471f77f58
rakesh468 <rakeshkumarg2000@gmail.com>
2022-01-24 07:34:12 -0800

    first commit

--- helper.js
+++ helper.js
@@ -1,38 +1,61 @@
 import { client } from "./index.js";
 import bcrypt from "bcrypt";
+import { ObjectId } from "mongodb";
 
- async function DeleteMovieById(id) {
-    return await client.db("movieapp").collection("movies").deleteOne({ id: id });
+async function DeleteMovieById(id) {
+  return await client
+    .db("movieapp")
+    .collection("movies")
+    .deleteOne({ _id: ObjectId(id) });
 }
- async function UpdateMovieById(id, data) {
-    return await client.db("movieapp").collection("movie").updateOne({ id: id }, { $set: data });
+async function UpdateMovieById(id, data) {
+  return await client
+    .db("movieapp")
+    .collection("movie")
+    .updateOne({ _id: ObjectId(id) }, { $set: data });
 }
- async function CreateMovie(data) {
-    return await client.db("movieapp").collection("movies").insertMany(data);
+async function CreateMovie(data) {
+  return await client.db("movieapp").collection("movies").insertMany(data);
 }
- async function FilterMovie(filter) {
-    return await client.db("movieapp").collection("movies").find(filter).toArray();
+async function FilterMovie(filter) {
+  return await client
+    .db("movieapp")
+    .collection("movies")
+    .find(filter)
+    .toArray();
 }
- async function GetmovieById(id) {
-    return await client.db("movieapp").collection("movies").findOne({ id: id });
+async function GetmovieById(id) {
+  return await client
+    .db("movieapp")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 async function CreateUser(data) {
-    return await client.db("movieapp").collection("users").insertOne(data);
-  }
-  async function Getuserbyname(email) {
-    return await client
-      .db("movieapp")
-      .collection("users")
-      .findOne({ email: email });
-  }
-
-async function genpassword(password){
-    const no_of_round=10;
-    const salt=await bcrypt.genSalt(no_of_round)
-    console.log(salt);
-    const hashedpassword=await bcrypt.hash(password,salt)
-    console.log(hashedpassword);
-    return hashedpassword;
+  return await client.db("movieapp").collection("users").insertOne(data);
+}
+async function Getuserbyname(email) {
+  return await client
+    .db("movieapp")
+    .collection("users")
+    .findOne({ email: email });
 }
 
-export  { GetmovieById, FilterMovie, CreateMovie, UpdateMovieById, DeleteMovieById,genpassword,CreateUser,Getuserbyname }
+async function genpassword(password) {
+  const no_of_round = 10;
+  const salt = await bcrypt.genSalt(no_of_round);
+  console.log(salt);
+  const hashedpassword = await bcrypt.hash(password, salt);
+  console.log(hashedpassword);
+  return hashedpassword;
+}
+
+export {
+  GetmovieById,
+  FilterMovie,
+  CreateMovie,
+  UpdateMovieById,
+  DeleteMovieById,
+  genpassword,
+  CreateUser,
+  Getuserbyname,
+};
--- index.js
+++ index.js
@@ -2,38 +2,34 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import { MoviesRouter } from "./routes/movies.js";
-import {UserRouter} from "./routes/users.js";
+import { UserRouter } from "./routes/users.js";
 
-dotenv.config();//dotenv configiration//
+dotenv.config(); //dotenv configiration//
 
-const app=express()
+const app = express();
 
-const PORT=process.env.PORT //PORT Number//
+const PORT = process.env.PORT; //PORT Number//
 
 app.use(express.json()); //middleware to convert data into json format//
 
-const MONGO_URL=process.env.MONGO_URL //MongoDb URL//
-
-
+const MONGO_URL = process.env.MONGO_URL; //MongoDb URL//
 
 //MongoDb connection//
-async function Connection(){
-    const client=new MongoClient(MONGO_URL)
-    await client.connect()
-    console.log("Mongodb connected")
-    return client;
+async function Connection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongodb connected");
+  return client;
 }
 
-export const client=await Connection();
+export const client = await Connection();
 
 // server welcome page //
-app.get("/",(request,response)=>{
-response.send("Hello World")
-})
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
 
-app.use("/movies",MoviesRouter)
-app.use("/users",UserRouter)
+app.use("/movies", MoviesRouter);
+app.use("/users", UserRouter);
 
-app.listen(PORT,()=>console.log("APP running in PORT",PORT));
-
-
+app.listen(PORT, () => console.log("APP running in PORT", PORT));
