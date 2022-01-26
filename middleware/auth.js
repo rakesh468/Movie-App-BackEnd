af82d2ca71dc02a780b46da31f595bc516e8c426
rakesh468 <rakeshkumarg2000@gmail.com>
2022-01-26 00:11:07 -0800

    first commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ export const auth=(request,response,next)=>{
 try{
     const token=request.header("X-auth-token")
     console.log("token",token)
-    jwt.verify(token,process.env.SECRET_KEY)
+    jwt.verify(token,process.env.SECRET_KEY);
     next();
 }catch(err){
     response.status(401).send({message: err.message})
