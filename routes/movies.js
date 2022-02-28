2e07072b1b7b3b42734d91a3e42d53c5a74a0382
rakesh468 <rakeshkumarg2000@gmail.com>
2022-02-28 01:50:03 -0800

    first commit

--- routes/movies.js
+++ routes/movies.js
@@ -47,7 +47,7 @@ router
     response.send(movie);
    })//Edit movies using PUT Method//
 
-.delete(async(request,response)=>{
+.delete(auth,async(request,response)=>{
     const{id}=request.params;
     const result=await DeleteMovieById(id)
     result.deletedCount > 0
