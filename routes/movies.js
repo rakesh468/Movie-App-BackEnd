578b9ff3fe56b39dd32d8d0022a977ad1bcb8fe4
rakesh468 <rakeshkumarg2000@gmail.com>
2022-02-28 00:14:36 -0800

    first commit

--- routes/movies.js
+++ routes/movies.js
@@ -9,7 +9,7 @@ const router=express.Router()
 // To filter Movie By Rating using get method /
 router
 .route("/")
-.get(async(request,response)=>{
+.get(auth,async(request,response)=>{
     const filter=request.query;
     console.log(filter);
     if(filter.rating){
@@ -19,11 +19,11 @@ router
     response.send(filterrating);
     console.log(filterrating)
 })
-.get(async(request,response)=>{
+.get(auth,async(request,response)=>{
     const data=await Getmovies()
     response.send(data);
 })
-.post(async(request,response)=>{
+.post(auth,async(request,response)=>{
     const data=request.body;
     const movie=await CreateMovie(data)
     response.send(movie)
@@ -33,13 +33,13 @@ router
 // To get movies by id using get method //
 router
 .route("/:id")
-.get(async(request,response)=>{
+.get(auth,async(request,response)=>{
     const {id}=request.params;
     const result=await GetmovieById(id)
     response.send(result);
     console.log(result);
 })
-.put(async(request,response)=>{
+.put(auth,async(request,response)=>{
     const { id }=request.params;
     const data=request.body;
     const result=await UpdateMovieById(id, data);
@@ -47,7 +47,7 @@ router
     response.send(movie);
    })//Edit movies using PUT Method//
 
-.delete(async(request,response)=>{
+.delete(auth,async(request,response)=>{
     const{id}=request.params;
     const result=await DeleteMovieById(id)
     result.deletedCount > 0
